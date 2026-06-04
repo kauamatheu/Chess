60afd579fc080cb34fac575a3e0503741cebddbc
kauamatheu <kauamatheu407@gmail.com>
2026-06-03 23:12:04 -0300

    Começo do tabuleiro de xadrez, definindo os atributos do Pawn.

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        PrintaTabuleiro();
+    }
+
+    public static void PrintaTabuleiro() {
+        String [][] tabuleiro ={
+            {"T", "C", "B", "K", "R", "B", "C", "T"},
+            {"P", "P", "P", "P", "P", "P", "P", "P"},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {"p", "p", "p", "p", "p", "p", "p", "p"},
+            {"t", "c", "b", "k", "r", "b", "c", "t"}
+        };
+        
+        System.out.println("  A B C D E F G H");
+        for (int line = 0; line < 8; line++) {
+            System.out.print((line+1) + " ");
+            for (int col = 0; col < 8; col++) {
+                System.out.print(tabuleiro[line][col] + " ");
+            }
+            System.out.println("");
+        }
+    }
+        
+}
---
+++ pecas/Pawn.java
@@ -0,0 +1,11 @@
+package pecas;
+public class Pawn{
+    private int x_coordinate; 
+    private int y_coordinate;
+    private String color;
+
+    public Pawn(){
+
+    }
+
+}
