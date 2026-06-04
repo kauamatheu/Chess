397a434f989f4972496def98c119457183312c80
kauamatheu <kauamatheu407@gmail.com>
2026-06-04 14:29:22 -0300

    Inicio do desenvolvimento do metodo de movimentação da classe Pawn.

--- pieces/Pawn.java
+++ pieces/Pawn.java
@@ -4,19 +4,19 @@ public class Pawn{
     private int y_coordinate;
     private String color;
 
-    public Pawn(){
-
+    public Pawn(int x_coordinate, int y_coordinate, String color){
+        this.x_coordinate = x_coordinate;
+        this.y_coordinate = y_coordinate;
+        this.color = color;
     }
 
     // Getters
     public int get_xcoor(){
         return this.x_coordinate;
     }
-
     public int get_ycoor(){
         return this.y_coordinate;
     }
-
     public String get_color(){
         return this.color;
     }
@@ -25,15 +25,22 @@ public class Pawn{
     public void  set_xcoor(int x_coordinate){
         this.x_coordinate = x_coordinate;
     }
-
     public void set_ycoor(int y_coordinate){
         this.y_coordinate = y_coordinate;
     }
-
     public void set_color(String color){
         this.color = color;
     }
 
-    
+    public int movementX(int x_coordinate, String color){
+        int new_x_coordinate = 0;
 
+        if(color.equals("black") == true){
+            new_x_coordinate = x_coordinate-1;
+        }else{
+            new_x_coordinate = x_coordinate+1;
+        }
+        
+        return new_x_coordinate;
+    }
 }
